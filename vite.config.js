2587997bd883b722bf07f4690b2fda1404467dab
Sergiyss <weert19933@gmail.com>
2025-06-06 13:53:40 +0300

    Портфолио v3

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,10 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
 export default defineConfig({
-  plugins: [
-      vue(),
-      tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),  // Устанавливаем алиас для @
-    },
-  },
-})
+    plugins: [vue()],
+});
+

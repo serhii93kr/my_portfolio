671335060f346590173cd9c329d19522f5255075
Sergiyss <weert19933@gmail.com>
2025-06-06 13:55:53 +0300

    Портфолио v4

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path';
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [vue()],
 });
-

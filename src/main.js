f073896e36a913bf3fe239625a8ff27c833368bb
Sergiyss <weert19933@gmail.com>
2025-09-29 21:32:02 +0300

    Портфолио v24

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import i18n from './i18n';
 import { createPinia } from 'pinia';
 import { createHead } from '@vueuse/head';
 
-
 import './assets/styles/main.css';
 import './assets/styles/desktop.css';
 import './assets/styles/table.css';
@@ -17,16 +16,5 @@ app.use(createPinia());
 app.use(router);
 app.use(i18n);
 app.use(createHead());
-
-// Конфигурация Vue MQ
-app.use({
-  breakpoints: {
-    mobile: 768,
-    tablet: 1024,
-    desktop: 1280,
-    widescreen: 1920
-  },
-  defaultBreakpoint: 'mobile'
-});
 
 app.mount('#app');

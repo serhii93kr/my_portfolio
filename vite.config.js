245037d70276cd11e2267e9a2c3e23c1796f7744
Sergiyss <weert19933@gmail.com>
2025-09-29 21:19:40 +0300

    Портфолио v20

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [vue(),  tailwindcss(),],
+    plugins: [vue(), tailwindcss()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
+    },
+    build: {
+        sourcemap: true, // включаем source maps
     },
 });
